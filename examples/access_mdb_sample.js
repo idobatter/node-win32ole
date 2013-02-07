c1b703eb7e7e872c96902ae7d9aaef2d5da43fbb
idobatter <idobatter@gmail.com>
2013-02-07 16:04:06 +0900

    modify examples/access_mdb_sample.js before test to call win32ole.client.Dispatch() twice (now it is bug)

--- examples/access_mdb_sample.js
+++ examples/access_mdb_sample.js
@@ -9,46 +9,11 @@ var tmpdir = path.join(cwd, 'test/tmp');
 if(!fs.existsSync(tmpdir)) fs.mkdirSync(tmpdir);
 var outfile = path.join(tmpdir, 'access_mdb_sample.mdb');
 
-var adox_sample = function(filename){
-  console.log('creating mdb (by ADOX): "' + filename + '" ...');
-  var dsn = 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' + filename + ';';
-  // dsn += ('Locale Identifier=' + CAT_LOCALE_(language));
-  var db = win32ole.client.Dispatch('ADOX.Catalog', '.ACP'); // locale
-  db.call('Create', [dsn]);
-  var sql_create_table = 'create table testtbl (id autoincrement primary key,';
-  sql_create_table += ' c1 varchar(255), c2 integer, c3 varchar(255));';
-  var cn = db.get('ActiveConnection');
-  cn.call('Execute', [sql_create_table]);
-  /* this version can not create OLE instance at the same time
-  var rs = win32ole.client.Dispatch('ADODB.Recordset', '.ACP'); // locale
-  rs.set('ActiveConnection', cn); // supports to set Object ?
-  rs.call('Open', ['', , ,]); // supports empty arguments ?
-  rs.call('Close');
-  rs = null;
-  */
-  cn.call('Close');
-  cn = null;
-  // db.call('Close'); // 'Close' 'Disconnect' 'Release' is wrong
-  db = null;
-};
-
-var ole_automation_sample = function(filename){
-  console.log('open mdb (by OLE Automation): "' + filename + '" ...');
-  var mdb = win32ole.client.Dispatch('Access.Application', '.ACP'); // locale
-  mdb.set('Visible', true);
-  // mdb.call('NewCurrentDatabase', [filename]); // to create new mdb
-  mdb.call('OpenCurrentDatabase', [filename]); // to open exists mdb
-  var cdb = mdb.call('CurrentDb');
-  for(var i = 101; i < 109; ++i){
-    var sql_insert = "insert into testtbl (c1, c2, c3) values";
-    sql_insert += " ('p(', " + i + ", ')q');";
-    cdb.call('Execute', [sql_insert]);
-  }
+var display_and_edit_all = function(rs){
   var getRSvalue = function(rs, fieldname){
     return rs.get('Fields', [fieldname]).get('Value');
   }
-  var sql_select = 'select * from testtbl;';
-  var rs = cdb.call('OpenRecordset', [sql_select]);
+  rs.call('MoveFirst');
   while(!rs.get('Eof').toBoolean()){
     win32ole.print('id: ');
     win32ole.print(getRSvalue(rs, 'id').toInt32());
@@ -64,11 +29,56 @@ var ole_automation_sample = function(filename){
     rs.call('Update'); // rs.call('CancelUpdate');
     rs.call('MoveNext');
   }
+};
+
+var adox_sample = function(filename){
+  if(fs.existsSync(filename)) fs.unlinkSync(filename);
+  console.log('creating mdb (by ADOX): "' + filename + '" ...');
+  var dsn = 'Provider=Microsoft.Jet.OLEDB.4.0;Data Source=' + filename + ';';
+  // dsn += ('Locale Identifier=' + CAT_LOCALE_(language));
+  var db = win32ole.client.Dispatch('ADOX.Catalog', '.ACP'); // locale
+  db.call('Create', [dsn]);
+  var sql_create_table = 'create table testtbl (id autoincrement primary key,';
+  sql_create_table += ' c1 varchar(255), c2 integer, c3 varchar(255));';
+  var cn = db.get('ActiveConnection');
+  cn.call('Execute', [sql_create_table]);
+  /* this version can not create OLE instance at the same time
+  var rs = win32ole.client.Dispatch('ADODB.Recordset', '.ACP'); // locale
+  rs.set('ActiveConnection', cn);
+  var sql_select = 'select * from testtbl;';
+  rs.call('Open', [sql_select]); // default options
+  display_and_edit_all(rs);
+  rs.call('Close');
+  rs = null;
+  */
+  cn.call('Close');
+  cn = null;
+  // db.call('Close'); // 'Close' 'Disconnect' 'Release' is wrong
+  db = null;
+  console.log('disconnected (ADOX)');
+};
+
+var ole_automation_sample = function(filename){
+  console.log('open mdb (by OLE Automation): "' + filename + '" ...');
+  var mdb = win32ole.client.Dispatch('Access.Application', '.ACP'); // locale
+  mdb.set('Visible', true);
+  // mdb.call('NewCurrentDatabase', [filename]); // to create new mdb
+  mdb.call('OpenCurrentDatabase', [filename]); // to open exists mdb
+  var cdb = mdb.call('CurrentDb');
+  for(var i = 101; i < 109; ++i){
+    var sql_insert = "insert into testtbl (c1, c2, c3) values";
+    sql_insert += " ('p(', " + i + ", ')q');";
+    cdb.call('Execute', [sql_insert]);
+  }
+  var sql_select = 'select * from testtbl;';
+  var rs = cdb.call('OpenRecordset', [sql_select]);
+  display_and_edit_all(rs);
   rs.call('Close');
   cdb.call('Close');
   mdb.call('CloseCurrentDatabase');
   cdb = null;
   mdb = null;
+  console.log('disconnected (OLE Automation)');
 };
 
 win32ole.client = new win32ole.Client;
